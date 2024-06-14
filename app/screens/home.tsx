85d25b7dc8c1ff1049681e54951592b4a7920fcc
Bashamega <adambashaahmednaji@gmail.com>
2024-06-14 11:58:56 +0300

    Gallery 🚀🔥

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -2,15 +2,18 @@ import { Menu } from "../components/Menu";
 import { Div } from "../components/TailwindComponents";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Filter } from "../components/filter";
+import { Gal } from "../components/gal";
 export function Home() {
     const insets = useSafeAreaInsets();
     return (
         <Div className="relative h-full w-screen" style={{
             paddingTop: insets.top,
             paddingBottom: insets.bottom,
-        }}>
+        }}>                
+            <Filter />
+
             <Div className="p-5">
-                <Filter/>
+                <Gal />
             </Div>
             <Menu />
         </Div>
