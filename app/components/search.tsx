824d744074de5c24485db6318e71400102d3627e
Bashamega <adambashaahmednaji@gmail.com>
2024-06-14 11:43:45 +0300

    Filter update 🔥

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -4,8 +4,7 @@ import { Div, Input } from "./TailwindComponents";
 export function Search(){
     return(
         <Div className=" flex-row justify-between w-4/5 border-[#F69F21] border p-2 rounded-lg">
-            <Input placeholder="search" className="b-1 w-2/4"/>
-            <Div className="w-1 h-full bg-[#DCDFE3]"/>
+            <Input placeholder="search" className=" w-3/4 border-r-[1.7px] border-[#DCDFE3]"/>
             <Text>Icon</Text>
         </Div>
     )
