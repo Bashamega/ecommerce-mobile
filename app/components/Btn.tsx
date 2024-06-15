4587ad6e96c1b5c87650e6ac65edf1c7d90c37ce
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 08:49:47 +0300

    Increased the radius of the button

--- app/components/Btn.tsx
+++ app/components/Btn.tsx
@@ -2,7 +2,7 @@ import { Touch, P } from "./TailwindComponents";
 
 export function Button({ onPress, title }: { onPress: () => void, title: string }) {
     return (
-        <Touch onPress={onPress} className="bg-[#F69F21] p-3 rounded">
+        <Touch onPress={onPress} className="bg-[#F69F21] p-3 rounded-lg">
             <P className="text-white text-center font-bold">{title}</P>
         </Touch>
     );
