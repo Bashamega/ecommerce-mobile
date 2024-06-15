d499a911456c0745ee8be8943c64d73cc202a5a7
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 07:29:27 +0300

    Button 🔥

---
+++ app/components/Btn.tsx
@@ -0,0 +1,9 @@
+import { Touch, P } from "./TailwindComponents";
+
+export function Button({ onPress, title }: { onPress: () => void, title: string }) {
+    return (
+        <Touch onPress={onPress} className="bg-[#F69F21] p-3 rounded">
+            <P className="text-white text-center font-bold">{title}</P>
+        </Touch>
+    );
+}
