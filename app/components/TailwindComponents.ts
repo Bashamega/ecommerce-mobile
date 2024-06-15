e1eb06b95337007726dc54e7301d2ace2aea576e
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 06:17:16 +0300

    New Talwin component 🔥

--- app/components/TailwindComponents.ts
+++ app/components/TailwindComponents.ts
@@ -1,5 +1,5 @@
 import { styled } from "nativewind";
-import { Image, ScrollView, Text, TextInput, View } from "react-native";
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 export const Div = styled(View)
 export const Input = styled(TextInput)
@@ -7,3 +7,4 @@ export const P = styled(Text)
 export const Scroll = styled(ScrollView)
 export const Img = styled(Image)
 export const Icon = styled(Ionicons)
+export const Touch = styled(TouchableOpacity)
