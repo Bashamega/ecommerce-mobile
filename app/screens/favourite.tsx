e89c6b7b1ea208ccd18c248f71c261433f5160ac
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 07:16:49 +0300

    Added more products 🔥

--- app/screens/favourite.tsx
+++ app/screens/favourite.tsx
@@ -3,16 +3,19 @@ import { Nav } from "../components/Nav";
 import { Div } from "../components/TailwindComponents";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Item } from "../components/Item";
-export function Favourite({navigation}:{navigation:any}){
+export function Favourite({ navigation }: { navigation: any }) {
     const insets = useSafeAreaInsets();
-    return(
+    return (
         <Div className="relative h-full w-screen bg-[#FFFBF5]" style={{
             paddingTop: insets.top,
             paddingBottom: insets.bottom,
         }}>
-            <Nav text="Favourite" navigation={navigation}/>
+            <Nav text="Favourite" navigation={navigation} />
             <Div className=" p-10">
-                <Item/>
+                <Item disabled={false} />
+                <Item disabled={false} />
+                <Item disabled={true} />
+                <Item disabled={false} />
             </Div>
             <Menu navigation={navigation} active={2} />
         </Div>
