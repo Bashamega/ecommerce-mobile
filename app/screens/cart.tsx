8bd6a1b8c6b412ea4fceb9cec0055c3ba91b62f1
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 08:59:35 +0300

    Checkout screen 🔥

--- app/screens/cart.tsx
+++ app/screens/cart.tsx
@@ -1,9 +1,38 @@
-import { View } from "react-native";
-
-export function Cart(){
-    return(
-        <View>
-            
-        </View>
+import { Menu } from "../components/Menu";
+import { Nav } from "../components/Nav";
+import { Div, Icon, P} from "../components/TailwindComponents";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button } from "../components/Btn";
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
+export function Cart({ navigation }: { navigation: any }) {
+    const insets = useSafeAreaInsets();
+    return (
+        <Div className="relative h-full w-screen bg-[#FFFBF5]" style={{
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+        }}>
+                <Nav text="Checkout" navigation={navigation} />
+                <Div className="p-5">
+                    <Div className=" flex-row mb-5  p-5 border border-[grey] rounded-2xl">
+                        <FontAwesome5 name="credit-card" size={30} color="grey" />
+                        <P className=" font-bold ml-2">Credit or Debit</P>
+                    </Div>
+                    <Div className=" flex-row mb-5  p-5 border border-[#F69F21] rounded-2xl">
+                        <FontAwesome5 name="money-bill" size={30} color="#F69F21" />
+                        <P className=" font-bold ml-2">Mobile Money</P>
+                        <Icon name="checkmark-circle" size={30} className=" absolute right-[10px] top-[50%]" color="#F69F21"/>
+                    </Div>
+                    <Div className=" flex-row mb-5  p-5 border border-[grey] rounded-2xl">
+                        <FontAwesome name="bank" size={30} color="grey" />
+                        <P className=" font-bold ml-2">Bank Transfer</P>
+                    </Div>
+                    <Div className=" flex-row mb-[200px]  p-5 border border-[grey] rounded-2xl">
+                        <FontAwesome5 name="store" size={30} color="grey" />
+                        <P className=" font-bold ml-2">Pay in store</P>
+                    </Div>
+                    <Button title="Buy now" onPress={()=>{navigation?.navigate("check")}}/>
+                </Div>
+            <Menu navigation={navigation} active={4} />
+        </Div>
     )
 }
