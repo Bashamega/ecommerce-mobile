650dc33d3968649d5cadf4a304e32a71d94e6d5f
Bashamega <adambashaahmednaji@gmail.com>
2024-06-15 07:09:07 +0300

    Favourite screen Update 🔥🔥

--- app/screens/favourite.tsx
+++ app/screens/favourite.tsx
@@ -1,9 +1,20 @@
-import { View } from "react-native";
-
-export function Favourite(){
+import { Menu } from "../components/Menu";
+import { Nav } from "../components/Nav";
+import { Div } from "../components/TailwindComponents";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Item } from "../components/Item";
+export function Favourite({navigation}:{navigation:any}){
+    const insets = useSafeAreaInsets();
     return(
-        <View>
-            
-        </View>
+        <Div className="relative h-full w-screen bg-[#FFFBF5]" style={{
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+        }}>
+            <Nav text="Favourite" navigation={navigation}/>
+            <Div className=" p-10">
+                <Item/>
+            </Div>
+            <Menu navigation={navigation} active={2} />
+        </Div>
     )
 }
